6d533162742a1b1871f31f4016d6ae3c43bb044e
jonax <jonaxwork@outlook.com>
2023-10-10 12:44:05 +0800

    init and parse number

---
+++ src/Parser.js
@@ -0,0 +1,37 @@
+// parser: recursive descent implementation
+class Parser {
+    /**
+     * parses a string into an AST.
+     */
+    parse(string) {
+        //
+        this._string = string
+        return this.Program()
+    }
+
+    /**
+     * Main entry point.
+     *
+     * Program:
+     *  :NumericLiteral
+     *  ;
+     */
+    Program() {
+        return this.NumericLiteral();
+    }
+
+    /**
+     * NumericLiteral
+     *  : NUMBER
+     *  ;
+     */
+    NumericLiteral() {
+        return {
+            type: "NumericLiteral", value: Number(this._string)
+        }
+    }
+}
+
+module.exports = {
+    Parser,
+}
