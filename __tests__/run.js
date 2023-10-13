9c531564a7f4a71a2ceaca5981faa8157045ed68
Jonax <jonaxwork@outlook.com>
2023-10-13 11:04:47 +0800

    tdd mode

--- __tests__/run.js
+++ __tests__/run.js
@@ -2,16 +2,36 @@
  * Main test number
  */
 const {Parser} = require("../src/Parser")
+const assert = require("assert")
+
+/**
+ * List of tests
+ */
+const tests = [require("./literals-test")]
 
 const parser = new Parser()
 
-const program = `
+function exec() {
+    const program = `
+    /**
+    * numbewr
+    */
+                    42
+    `
+
+    const ast = parser.parse(program)
+
+    console.log(JSON.stringify(ast, null, 2))
+}
+
 /**
-* numbewr
-*/
-                42
-`
+ * Test function
+ */
+function test(program, expected) {
+    const ast = parser.parse(program)
+    assert.deepEqual(ast, expected)
+}
 
-const ast = parser.parse(program)
-
-console.log(JSON.stringify(ast, null, 2))
+// Run al tests
+tests.forEach((testRun) => testRun(test))
+console.log("All assertions passed")
